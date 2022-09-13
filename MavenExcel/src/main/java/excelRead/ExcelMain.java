1da0c165fd0f89cdf4f5c87a31e9e7738fd2cdb7
anjub <anjub@anjubenny>
2022-09-13 16:59:52 +0530

    first commit

---
+++ MavenExcel/src/main/java/excelRead/ExcelMain.java
@@ -0,0 +1,18 @@
+package excelRead;
+
+import java.io.IOException;
+
+public class ExcelMain {
+
+	public static void main(String[] args) throws IOException
+	{
+		String s = ExcelCode.readStringData(0, 1);
+		System.out.println(s);
+		
+		String s1 = ExcelCode.readIntegerData(0, 0);
+		System.out.println(s1);
+
+		
+	}
+
+}
